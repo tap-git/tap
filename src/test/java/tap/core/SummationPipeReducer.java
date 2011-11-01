9ad251ea1ce10799371dfc962e02b91031c87c79
Douglas Moore <Douglas.Moore@ThinkBigAnalytics.com>
2011-11-01 12:49:12 -0400

    Explore reducer operation in summation test.

--- src/test/java/tap/core/SummationPipeReducer.java
+++ src/test/java/tap/core/SummationPipeReducer.java
@@ -7,15 +7,19 @@ package tap.core;
 public class SummationPipeReducer extends
 		BaseReducer<CountRec, OutputLog> {
 
-    private OutputLog outLog = new OutputLog();
+    private OutputLog outLog = new OutputLog("sum of words", 0);
+    
 	@Override
 	public void reduce(Pipe<CountRec> in, Pipe<OutputLog> out) {
-	    
-	    outLog.description = "sum of words";
-	    outLog.count = 0;
+	    int loopCount = 0;
+	    CountRec val;
 	    while (in.hasNext()) {
-	        outLog.count += in.next().count;
+	        val = in.next();
+	        System.out.printf("<CountRec> (%s, %d) \n", val.word, val.count);
+	        outLog.count += val.count;
+	        loopCount ++;
 	    }
+	    System.out.printf("SumationPipeReducer: Loop Count=%d Outputing outlog.count = %d\n", loopCount, outLog.count);
 		out.put(outLog);
 	}
 }
