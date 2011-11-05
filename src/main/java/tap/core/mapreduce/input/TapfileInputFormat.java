21674cc374a181753371a292a5c777232dcae3af
Ajai Shankar <ajai.shankar@thinkbiganalytics.com>
2011-11-05 13:01:26 -0700

    Tapfile input & output format

--- src/main/java/tap/core/mapreduce/input/TapfileInputFormat.java
+++ src/main/java/tap/core/mapreduce/input/TapfileInputFormat.java
@@ -1,5 +1,38 @@
 package tap.core.mapreduce.input;
 
-public class TapfileInputFormat {
+import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.InputSplit;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapred.Reporter;
+
+import com.google.protobuf.Message;
+import com.twitter.elephantbird.mapreduce.io.BinaryWritable;
+import com.twitter.elephantbird.util.Protobufs;
+import com.twitter.elephantbird.util.TypeRef;
+
+public class TapfileInputFormat<M extends Message> extends FileInputFormat<LongWritable, BinaryWritable<M>>{
+    private TypeRef<M> typeRef;
+
+    @SuppressWarnings("rawtypes")
+    public static <M extends Message> Class<TapfileInputFormat>
+       getInputFormatClass(Class<M> protoClass, Configuration job) {
+      Protobufs.setClassConf(job, TapfileInputFormat.class, protoClass);
+      return TapfileInputFormat.class;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public RecordReader<LongWritable, BinaryWritable<M>> getRecordReader(
+            InputSplit genericSplit, JobConf job, Reporter reporter) throws IOException {
+        if (typeRef == null) {
+            typeRef = Protobufs.getTypeRef(job, TapfileInputFormat.class);
+        }
+        return new TapfileRecordReader<M>(job, (FileSplit) genericSplit, typeRef);
+    }
 }
--- src/main/java/tap/core/mapreduce/output/TapfileOutputFormat.java
+++ src/main/java/tap/core/mapreduce/output/TapfileOutputFormat.java
@@ -1,5 +1,42 @@
 package tap.core.mapreduce.output;
 
-public class TapfileOutputFormat {
+import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordWriter;
+import org.apache.hadoop.util.Progressable;
+
+import com.google.protobuf.Message;
+import com.twitter.elephantbird.mapreduce.io.BinaryWritable;
+import com.twitter.elephantbird.util.Protobufs;
+import com.twitter.elephantbird.util.TypeRef;
+
+public class TapfileOutputFormat<M extends Message> extends FileOutputFormat<NullWritable, BinaryWritable<M>>{
+
+    private TypeRef<M> typeRef;
+
+    @SuppressWarnings("rawtypes")
+    public static <M extends Message> Class<TapfileOutputFormat>
+       getInputFormatClass(Class<M> protoClass, Configuration job) {
+      Protobufs.setClassConf(job, TapfileOutputFormat.class, protoClass);
+      return TapfileOutputFormat.class;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public RecordWriter<NullWritable, BinaryWritable<M>> getRecordWriter(
+            FileSystem fileSystem, JobConf job, String name, Progressable progressable)
+            throws IOException {
+        if (typeRef == null) {
+            typeRef = Protobufs.getTypeRef(job, TapfileOutputFormat.class);
+        }
+        Path path = FileOutputFormat.getTaskOutputPath(job, name);
+        return new TapfileRecordWriter<M>(job, path, progressable, typeRef);
+    }
 }
