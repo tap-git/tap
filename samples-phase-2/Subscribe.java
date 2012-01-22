a8aa5dbdfc7094baef37d193cfc03dd1c38a703d
Paul Sutter <psutter@quantbench.com>
2012-01-22 15:06:02 -0800

    add tap pahse 2 samples

---
+++ samples-phase-2/Subscribe.java
@@ -0,0 +1,19 @@
+package tap.sample;
+
+import tap.*;
+import quantbench.Candle;
+
+public class Subscribe extends Tap {
+    public static main(String[] args) throws Exception {
+        Tap tap = new Tap();
+        tap.startTime("2011-01-03 10:40:00.000");
+        tap.endTime("2011-01-03 10:50:00.000");
+        Pipe<Candle> candles = tap.subscribe("//cta/candle/1min[AAPL]");
+        while (candles.more()) {
+            Candle m = candles.get();
+            Date time = Tap.newDate(m.getStartTime());
+            System.out.println(time.toString + " symbol " + m.getSymbol() + 
+                               " open " + m.getOpen() + " volume " + m.getVolume());
+        }
+    }
+}
